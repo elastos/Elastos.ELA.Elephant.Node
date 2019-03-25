e789eea901edb74bd8a5aa143a152d196b50afdc
xiaoming <1406629722@qq.com>
2019-03-25 17:21:49 +0800

    take over all service of Elastos.ELA

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"github.com/elastos/Elastos.ELA.Elephant.Node/ela"
-	"github.com/elastos/Elastos.ELA.Elephant.Node/id"
 	"github.com/elastos/Elastos.ELA.Utility/signal"
 )
 
 func main() {
 	var interrupt = signal.NewInterrupt()
 	go ela.Go()
-	go id.Go()
+	//go id.Go()
 	<-interrupt.C
 }
